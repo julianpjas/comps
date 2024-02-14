4267b16c2d780e5f93fe08740d7acc32317b68de
Julian Jasionowski <julianpj1997@gmail.com>
2024-02-14 17:01:43 -0500

    Push 3

---
+++ src/Button.js
@@ -0,0 +1,8 @@
+function Button(){
+    return <button>Hi there!</button>;
+}
+
+// Underlying element is the actual JSX component
+// that is being created, like the button in this case.
+
+export default Button;
